6848ba6216d279d06023815cefe25a4f60b77d13
TANIGAWA Takashi <mihailjp@gmail.com>
2011-09-16 00:16:11 +0900

    added help file

--- Release/package.bat
+++ Release/package.bat
@@ -6,6 +6,7 @@ copy hsplzma.dll hsplzma
 copy hsplzmax.dll hsplzma
 copy hsplzma.as hsplzma
 copy hsplzmax.as hsplzma
+copy hsplzma.hs hsplzma
 copy sample.hsp hsplzma
 copy readme.txt hsplzma
 "C:\Program Files\7-Zip\7z.exe" a -mx=9 hsplzma.zip hsplzma
