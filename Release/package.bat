6419312ed30762ef4becf941ab1a8671c940db35
TANIGAWA Takashi <mihailjp@gmail.com>
2012-01-22 22:40:38 +0900

    バッチファイル再修正

--- Release/package.bat
+++ Release/package.bat
@@ -1,4 +1,5 @@
 rmdir /s /q hsplzma
+del /q hsplzma.zip
 mkdir hsplzma
 copy hsplzma.dll hsplzma
 copy hsplzmax.dll hsplzma
@@ -9,6 +10,6 @@ mkdir hsplzma\hsphelp
 copy hsplzma.hs hsplzma\hsphelp
 mkdir hsplzma\sample
 mkdir hsplzma\sample\hsplzma
-copy lzmaenc.hsp hsplzma
+copy lzmaenc.hsp hsplzma\sample\hsplzma
 copy readme.txt hsplzma
 "C:\Program Files\7-Zip\7z.exe" a -mx=9 hsplzma.zip hsplzma
